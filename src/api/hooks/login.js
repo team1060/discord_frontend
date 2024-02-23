ad54f1a4004d626f5e36794c6ef75fda6f3c712a
tesam1060 <team1060dev@gmail.com>
2024-02-24 00:57:33 +0900

    Fix: 로그인 및 로그아웃 수정

--- src/api/hooks/login.js
+++ src/api/hooks/login.js
@@ -3,20 +3,16 @@
 import { apiRequest } from '../../api/request';
 import { API_URL } from '../../api/urls';
 
-// 로그아웃 시 호출되는 함수
-export const handleLogout = () => {
-  // 액세스 토큰을 로컬 스토리지에서 제거
-  localStorage.removeItem('accessToken');
-};
-
 // 로그인 API 호출하는 함수
 export const login = async (email, password) => {
   try {
     const response = await apiRequest.post(API_URL.LOGIN, { email, password });
+    console.log(response.data);
     const { accessToken } = response.data;
 
     // 로그인 성공 시 액세스 토큰을 로컬 스토리지에 저장
     localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('email', email);
     return true;
   } catch (error) {
     console.error(error);

8d375faaf4f43e175dd3556124cfbd13fe3afaad
tesam1060 <team1060dev@gmail.com>
2024-03-05 14:53:42 +0900

    Refactor: UserInfo 개발에 의한 친구 리스트 및 리팩토링

--- src/components/PeopleListItem/PeopleListItem.jsx
+++ src/components/PeopleListItem/PeopleListItem.jsx
@@ -1,19 +1,13 @@
 import PropTypes from 'prop-types';
 import './PeopleListItem.scss';
-import Avatar from '../Avatar/Avatar';
 import { TbMessage2, TbDotsVertical } from 'react-icons/tb';
+import UserInfo from '../UserInfo/UserInfo';
 
 const PeopleListItem = ({ avatar, username, state, onClick }) => {
   return (
     <div className="people-item" onClick={onClick}>
       <div className="item-content">
-        <div className="user-info">
-          <Avatar avatar={avatar} />
-          <div className="text">
-            <div className="username">{username}</div>
-            <div className="subtext">{state}</div>
-          </div>
-        </div>
+        <UserInfo avatar={avatar} username={username} state={state} />
         <div className="action">
           <div className="action-button">
             <TbMessage2 size="24" />

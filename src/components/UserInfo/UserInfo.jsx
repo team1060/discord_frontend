9da6bd4bfaf45b825791a7a21472e541bd1d5a8c
tesam1060 <team1060dev@gmail.com>
2024-03-05 14:51:17 +0900

    Feat: ItemCard, UserInfo 작성

---
+++ src/components/UserInfo/UserInfo.jsx
@@ -0,0 +1,23 @@
+import Avatar from '../Avatar/Avatar';
+import PropTypes from 'prop-types';
+import './UserInfo.scss';
+
+const UserInfo = ({ avatar, username, state }) => {
+  return (
+    <div className="user-info">
+      <Avatar avatar={avatar} />
+      <div className="text">
+        <div className="username">{username}</div>
+        <div className="subtext">{state}</div>
+      </div>
+    </div>
+  );
+};
+
+UserInfo.propTypes = {
+  avatar: PropTypes.oneOfType([PropTypes.node, PropTypes.string]).isRequired,
+  username: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+};
+
+export default UserInfo;
